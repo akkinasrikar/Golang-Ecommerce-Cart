f9d1e6ea3413dc6d3c0ac521b8ea8ebdeddf4bcc
akkina.sairamsrikar <akkina.sairamsrikar@OPN-MC-388.local>
2023-07-02 16:02:37 +0530

    Generating token after login

--- repositories/repositories_gorm.go
+++ repositories/repositories_gorm.go
@@ -15,7 +15,8 @@ func (r *Repository) SignUp(userDetails entities.SignUp) (entities.SignUp, error
 // Login
 func (r *Repository) Login(userDetails entities.Login) (entities.Login, error) {
 	var user entities.Login
-	err := r.Db.Table("user_details").Where("user_name = ? AND user_password = ?", userDetails.Name, userDetails.Password).First(&user).Error
+	// compare user_name or user_email with userDetails.Name
+	err := r.Db.Table("user_details").Where("user_name = ? OR user_email = ? and user_password = ?", userDetails.Name, userDetails.Name, userDetails.Password).First(&user).Error
 	if err != nil {
 		return entities.Login{}, err
 	}
--- utils/utils.go
+++ utils/utils.go
@@ -1,1 +1,26 @@
 package utils
+
+import (
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+)
+
+func GenerateToken(Username string) (string, error) {
+	claims := jwt.MapClaims{
+		"sub": Username,
+		"exp": time.Now().Add(time.Minute * 15).Unix(),
+	}
+
+	signingMethod := jwt.SigningMethodHS256
+	secretKey := []byte("testing")
+
+	token := jwt.NewWithClaims(signingMethod, claims)
+
+	signedToken, err := token.SignedString(secretKey)
+	if err != nil {
+		return "", err
+	}
+
+	return signedToken, nil
+}
