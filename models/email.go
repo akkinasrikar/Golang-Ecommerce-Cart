f7515cd84785ea2eaeab200628cd231d9a3e6e4b
akkinasairamsrikar-open <akkina.sairamsrikar@bankopen.co>
2023-10-04 10:46:07 +0530

    Notify Delivery Status Via Email

--- models/email.go
+++ models/email.go
@@ -3,3 +3,10 @@ package models
 type OrderDetailsEmail struct {
 	OrderID string `json:"order_id"`
 }
+
+type SendEmailRequest struct {
+	Email       string `json:"email"`
+	Message     string `json:"message"`
+	Subject     string `json:"subject"`
+	ImageBase64 string `json:"image_base64"`
+}
